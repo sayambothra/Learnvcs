0dddd6f1ecc83947303838ca772b0c50e3bf51e4
Sayambothra <bothrasayam007@gmail.com>
2022-04-02 15:39:47 +0530

    Added bye class

--- Hello.java
+++ Hello.java
@@ -1,6 +1,13 @@
 public class Hello{
     public static void main(String args[]){
         system.out.println("Hello Git");
-         system.out.println("Git makes life easy");
+        system.out.println("Git makes life easy");
+        Bye b= new Bye();
+        b.wave();
+    }
+}
+class Bye{
+    public void wave(){
+        system.out.println("Wave bye")
     }
 }
